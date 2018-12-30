58ed2e665d5df75f4dc46f46ed19f6b6768e95b8
Theo P <teddypeifer@gmail.com>
2018-12-30 20:55:22 +0100

    Update STATICS.py

--- chat_system/client/STATICS.py
+++ chat_system/client/STATICS.py
@@ -4,10 +4,6 @@ VERSION = "1.0.0"
 GITHUB = "https://github.com/hollowcodes/"
 HOST = "hollocodes"
 
-#SERVER_IP = "127.0.0.1"#"192.168.2.118"
-SERVER_IP = "192.168.2.118"
-
-#CLIENT_IP = "127.0.0.1"#"192.168.2.101"
-CLIENT_IP = "192.168.2.101"
-
+SERVER_IP = "127.0.0.1"
+CLIENT_IP = "127.0.0.1"
 PORT = 6000
