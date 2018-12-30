1c5d29ecbc87cf126994d9f39954af68766ca031
Theo P <teddypeifer@gmail.com>
2018-12-30 20:43:23 +0100

    Delete create_register_once.py

--- chat_system/server/sql_database/create_register_once.py
+++
@@ -1,9 +0,0 @@
-from chat_system.server.sql_database import sql_handler
-
-# creates new sql database called registry (resets the entire database if it is already set up)
-
-
-def create_register():
-    d = sql_handler.database()
-    d.create_register_table()
-
